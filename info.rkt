3fcb1bb47c10c35fcebdccaa4554b3c303634504
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-05-15 14:29:55 -0400

    Fix dependencies

--- info.rkt
+++ info.rkt
@@ -1,4 +1,6 @@
 #lang setup/infotab
 
 (define collection "libkenji")
-(define deps '("base" "compatibility-lib" "data-lib" "srfi-lib" "pfds"))
+(define deps '("typed-racket-lib"
+               "unstable-contract-lib"
+               "base" "compatibility-lib" "data-lib" "srfi-lib" "pfds"))
